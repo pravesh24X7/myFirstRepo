e5d25303968b66cd1a27172dafe316147a51f00a
pravesh24X7 <84018906+pravesh24X7@users.noreply.github.com>
2021-06-24 13:36:50 +0530

    Add files via upload

--- first.cpp
+++ first.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <cstring>
 #include <fstream>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-
+int main(int argc, char const *argv[])
+{
     fstream my_file;
-    my_file.open("DEMO.txt", ios::in);
-    my_file.open("DEMO.txt", ios::binary);
+    my_file.open("DEMO.txt", ios::app);
 
     if (!my_file) {
-        cout << "You aren't Successfull";
+        cout << "file couldn't be opened !!" << endl;
     }
 
+    cout << "Enter data for file :" << endl;
     string str;
-    while (getline(my_file, str)) {
-        cout << str;
-    }
+    getline(cin, str);
+
+    my_file << str;
+
     my_file.close();
 
     return 0;
